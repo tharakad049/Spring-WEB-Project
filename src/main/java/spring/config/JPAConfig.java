f056717d2041a763c39417ba0de97a21a476a611
DilanTharaka <tharakad049@gmail.com>
2022-06-19 00:30:38 +0530

    Update Project

--- src/main/java/spring/config/JPAConfig.java
+++ src/main/java/spring/config/JPAConfig.java
@@ -1,4 +1,36 @@
 package spring.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class JPAConfig {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va){
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setJpaVendorAdapter(va);
+        bean.setJtaDataSource(ds);
+        bean.setPackagesToScan("spring.entity");
+        return bean;
+    }
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/SpringJPA?createDatabaseIfNotExist=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        return dataSource;
+    }
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter(){
+
+    }
 }
+
--- src/main/java/spring/config/WebAppConfig.java
+++ src/main/java/spring/config/WebAppConfig.java
@@ -1,4 +1,12 @@
 package spring.config;
 
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "spring")
 public class WebAppConfig {
 }
