71496f43ae4edf293fe2092401fb0ba2f6af849d
DilanTharaka <tharakad049@gmail.com>
2022-06-19 16:19:25 +0530

    Update project

--- src/main/java/lk/example/spring/entity/Customer.java
+++ src/main/java/lk/example/spring/entity/Customer.java
@@ -52,4 +52,14 @@ public class Customer {
     public void setSalary(double salary) {
         this.salary = salary;
     }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
---
+++ src/test/java/lk/example/spring/repo/CustomerRepoTest.java
@@ -0,0 +1,55 @@
+package lk.example.spring.repo;
+
+import lk.example.spring.config.JPAConfig;
+import lk.example.spring.entity.Customer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+@WebAppConfiguration
+@ContextConfiguration(classes = {JPAConfig.class})
+@ExtendWith(SpringExtension.class)
+
+
+class CustomerRepoTest {
+
+    @Autowired
+    CustomerRepo customerRepo;
+
+    @Test
+    public void saveCustomer(){
+        Customer customer1 = new Customer("C-001", "Dilan", "Kataragama", 1000000.00);
+        customerRepo.save(customer1);
+    }
+
+    @Test
+    public void getAllCustomers(){
+        List<Customer> all = customerRepo.findAll();
+        for (Customer customer : all) {
+            System.out.println(customer.toString());
+        }
+    }
+    @Test
+    public void searchCustomer(){
+        Optional<Customer> byId = customerRepo.findById("C-001");
+        boolean present = byId.isPresent();
+        System.out.println(present);
+
+        Customer customer = byId.get();
+        System.out.println(customer.toString());
+    }
+
+    @Test
+    public void deleteCustomer(){
+
+    }
+
+
+}
